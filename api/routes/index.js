06f19c3fb9837a0bae2cf8b634af548465626341
Bjorn Melin <bjornmelin16@gmail.com>
2025-07-20 17:24:47 -0600

    chore(reset): reset repo for new implementation

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const healthCheckRoute = require('./healthCheck');
-
-const router = express.Router();
-
-router.use('/health', healthCheckRoute);
-
-module.exports = router;
